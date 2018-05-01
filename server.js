c24dc2f9c35c23d28493a72984a32d0b119b4ebd
Unknown <jakash1997@gmail.com>
2018-05-01 12:23:53 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const brain = require('brain.js');
+
+const network = new brain.NeuralNetwork();
+
+network.train([
+    { input: { r: 0.62, g: 0.72, b: 0.88 }, output: { light: 1 } },
+    { input: { r: 0.1, g: 0.84, b: 0.72 }, output: { light: 1 } },
+    { input: { r: 0.33, g: 0.24, b: 0.29 }, output: { dark: 1 } },
+    { input: { r: 0.74, g: 0.78, b: 0.86 }, output: { light: 1 } },
+    { input: { r: 0.31, g: 0.35, b: 0.41 }, output: { dark: 1 } },
+    { input: {r: 1, g: 0.99, b: 0}, output: { light: 1 } },
+    { input: {r: 1, g: 0.42, b: 0.52}, output: { dark: 1 } }
+]);
+
+const result = brain.likely({ r: 0.33, g: 0.24, b: 0.29 }, network)
+console.log(result);
